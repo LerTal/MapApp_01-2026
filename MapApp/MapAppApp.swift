00e4bbf14e286e4560691adb88e3539a16bd6d4a
Tal Lerman <tal.lerman@binah.ai>
2026-01-08 20:55:20 +0200

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Tal L on 08/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
